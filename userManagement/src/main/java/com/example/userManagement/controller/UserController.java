691c424155f387ef15942306258f6ab3d4eb3344
PrasadBMalode <prasadsaix-workz@outlook.com>
2026-07-29 15:32:52 +0530

    CRUDE Operation Springboot

--- userManagement/src/main/java/com/example/userManagement/controller/UserController.java
+++ userManagement/src/main/java/com/example/userManagement/controller/UserController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3001")
+@CrossOrigin(origins = "*")
+
 public class UserController {
 
     @Autowired
